7ba675c4478e7ec1d1c0e9029d32e1c8f7492fb9
nigel <bynum1001@gmail.com>
2023-07-13 16:03:22 -0400

    Completed

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -1,36 +1,68 @@
 package io.zipcoder.microlabs.mastering_loops;
 
 
+
 public class NumberUtilities {
-    public static String getEvenNumbers(int start, int stop) {
-        return null;
+    public static boolean isOdd(int n) {
+        return (n % 2 != 0);
     }
+
+    public static String getEvenNumbers(int start, int stop) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = start; i < stop; i++) {
+            if (isOdd(i)) {
+                sb.append(i);
+            }
+        }
+        return sb.toString();
+    }
+
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String result = "";
+
+        for (int i = start; i < stop; i++) {
+            if (!isOdd(i)) {
+                result = result + i;
+            }
+        }
+        return result;
     }
 
 
-    public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
-    }
+    public static String getRange(int stop) {
 
-    public static String getRange(int start) {
-        return null;
+        return getRange(0, stop, 1);
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+
+        return getRange(start, stop, 1);
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        return getExponentiations(start, stop, step, 1);
     }
 
+    public static String getSquareNumbers(int start, int stop, int step) {
+
+        return getExponentiations(start, stop, step, 2);
+    }
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = start; i < stop; i += step) {
+            // for loop which multiplied i 'exponent' times
+            sb.append((int) Math.pow(i, exponent));
+        }
+        return sb.toString();
     }
+
 }
+
+
